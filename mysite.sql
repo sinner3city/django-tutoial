b4af125ea85e4854d28ac056c4f3c6d1d08f194a
sinner3city <krystianwojda@gmail.com>
2019-02-15 21:55:20 +0100

    tutorial step 3

---
+++ mysite.sql
@@ -0,0 +1,475 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 15 Lut 2019, 21:48
+-- Wersja serwera: 5.6.21
+-- Wersja PHP: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Baza danych: `mysite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `auth_group`
+--
+
+CREATE TABLE IF NOT EXISTS `auth_group` (
+`id` int(11) NOT NULL,
+  `name` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `auth_group_permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `auth_group_permissions` (
+`id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `auth_permission`
+--
+
+CREATE TABLE IF NOT EXISTS `auth_permission` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `auth_permission`
+--
+
+INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALUES
+(1, 'Can add log entry', 1, 'add_logentry'),
+(2, 'Can change log entry', 1, 'change_logentry'),
+(3, 'Can delete log entry', 1, 'delete_logentry'),
+(4, 'Can view log entry', 1, 'view_logentry'),
+(5, 'Can add permission', 2, 'add_permission'),
+(6, 'Can change permission', 2, 'change_permission'),
+(7, 'Can delete permission', 2, 'delete_permission'),
+(8, 'Can view permission', 2, 'view_permission'),
+(9, 'Can add group', 3, 'add_group'),
+(10, 'Can change group', 3, 'change_group'),
+(11, 'Can delete group', 3, 'delete_group'),
+(12, 'Can view group', 3, 'view_group'),
+(13, 'Can add user', 4, 'add_user'),
+(14, 'Can change user', 4, 'change_user'),
+(15, 'Can delete user', 4, 'delete_user'),
+(16, 'Can view user', 4, 'view_user'),
+(17, 'Can add content type', 5, 'add_contenttype'),
+(18, 'Can change content type', 5, 'change_contenttype'),
+(19, 'Can delete content type', 5, 'delete_contenttype'),
+(20, 'Can view content type', 5, 'view_contenttype'),
+(21, 'Can add session', 6, 'add_session'),
+(22, 'Can change session', 6, 'change_session'),
+(23, 'Can delete session', 6, 'delete_session'),
+(24, 'Can view session', 6, 'view_session'),
+(25, 'Can add choice', 7, 'add_choice'),
+(26, 'Can change choice', 7, 'change_choice'),
+(27, 'Can delete choice', 7, 'delete_choice'),
+(28, 'Can view choice', 7, 'view_choice'),
+(29, 'Can add question', 8, 'add_question'),
+(30, 'Can change question', 8, 'change_question'),
+(31, 'Can delete question', 8, 'delete_question'),
+(32, 'Can view question', 8, 'view_question');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `auth_user`
+--
+
+CREATE TABLE IF NOT EXISTS `auth_user` (
+`id` int(11) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `auth_user`
+--
+
+INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
+(1, 'pbkdf2_sha256$120000$JMCKe8eFiwHb$/ELoM4FV+M+BlADx1Sbblkz88dxyP+/YyokKyZKImp0=', '2019-02-15 15:58:06.241349', 1, 'kwojda', '', '', '', 1, 1, '2019-02-15 15:57:57.614860');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `auth_user_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `auth_user_groups` (
+`id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `auth_user_user_permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `auth_user_user_permissions` (
+`id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `django_admin_log`
+--
+
+CREATE TABLE IF NOT EXISTS `django_admin_log` (
+`id` int(11) NOT NULL,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL,
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `django_admin_log`
+--
+
+INSERT INTO `django_admin_log` (`id`, `action_time`, `object_id`, `object_repr`, `action_flag`, `change_message`, `content_type_id`, `user_id`) VALUES
+(1, '2019-02-15 16:12:16.938753', '2', 'What''s going on?', 1, '[{"added": {}}]', 8, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `django_content_type`
+--
+
+CREATE TABLE IF NOT EXISTS `django_content_type` (
+`id` int(11) NOT NULL,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `django_content_type`
+--
+
+INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
+(1, 'admin', 'logentry'),
+(3, 'auth', 'group'),
+(2, 'auth', 'permission'),
+(4, 'auth', 'user'),
+(5, 'contenttypes', 'contenttype'),
+(7, 'polls', 'choice'),
+(8, 'polls', 'question'),
+(6, 'sessions', 'session');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `django_migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `django_migrations` (
+`id` int(11) NOT NULL,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `django_migrations`
+--
+
+INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
+(1, 'contenttypes', '0001_initial', '2019-02-15 14:43:36.386257'),
+(2, 'auth', '0001_initial', '2019-02-15 14:43:37.130902'),
+(3, 'admin', '0001_initial', '2019-02-15 14:43:37.286260'),
+(4, 'admin', '0002_logentry_remove_auto_add', '2019-02-15 14:43:37.300902'),
+(5, 'admin', '0003_logentry_add_action_flag_choices', '2019-02-15 14:43:37.333885'),
+(6, 'contenttypes', '0002_remove_content_type_name', '2019-02-15 14:43:37.440132'),
+(7, 'auth', '0002_alter_permission_name_max_length', '2019-02-15 14:43:37.496476'),
+(8, 'auth', '0003_alter_user_email_max_length', '2019-02-15 14:43:37.551476'),
+(9, 'auth', '0004_alter_user_username_opts', '2019-02-15 14:43:37.565480'),
+(10, 'auth', '0005_alter_user_last_login_null', '2019-02-15 14:43:37.615477'),
+(11, 'auth', '0006_require_contenttypes_0002', '2019-02-15 14:43:37.620476'),
+(12, 'auth', '0007_alter_validators_add_error_messages', '2019-02-15 14:43:37.632476'),
+(13, 'auth', '0008_alter_user_username_max_length', '2019-02-15 14:43:37.686539'),
+(14, 'auth', '0009_alter_user_last_name_max_length', '2019-02-15 14:43:37.736482'),
+(15, 'sessions', '0001_initial', '2019-02-15 14:43:37.782484'),
+(16, 'polls', '0001_initial', '2019-02-15 15:12:11.232493');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `django_session`
+--
+
+CREATE TABLE IF NOT EXISTS `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `django_session`
+--
+
+INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
+('l9m9we19q5jrkoa1nlj081r1nbdvrbze', 'MDIyZTk3ZDRkNGQxYTc4OTNmMGJkZjY0MmViYjQ1MWJmZDYzODBiMzp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiJlOTc3ZTZlYTdmM2QzNjc5ZjU1YmUwNmEyMjRhODAzMjkxZTA2MTE5In0=', '2019-03-01 15:58:06.246349');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `polls_choice`
+--
+
+CREATE TABLE IF NOT EXISTS `polls_choice` (
+`id` int(11) NOT NULL,
+  `choice_text` varchar(200) NOT NULL,
+  `votes` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `polls_choice`
+--
+
+INSERT INTO `polls_choice` (`id`, `choice_text`, `votes`, `question_id`) VALUES
+(1, 'Not much', 0, 1),
+(2, 'The sky', 0, 1),
+(3, 'Just hacking again', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `polls_question`
+--
+
+CREATE TABLE IF NOT EXISTS `polls_question` (
+`id` int(11) NOT NULL,
+  `question_text` varchar(200) NOT NULL,
+  `pub_date` datetime(6) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `polls_question`
+--
+
+INSERT INTO `polls_question` (`id`, `question_text`, `pub_date`) VALUES
+(1, 'What''s new?', '2019-02-15 15:41:32.387131'),
+(2, 'What''s going on?', '2019-02-15 16:12:15.000000');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `auth_group`
+--
+ALTER TABLE `auth_group`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`), ADD KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`);
+
+--
+-- Indexes for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`);
+
+--
+-- Indexes for table `auth_user`
+--
+ALTER TABLE `auth_user`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`), ADD KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`);
+
+--
+-- Indexes for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`), ADD KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`);
+
+--
+-- Indexes for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+ ADD PRIMARY KEY (`id`), ADD KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`), ADD KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`);
+
+--
+-- Indexes for table `django_content_type`
+--
+ALTER TABLE `django_content_type`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`);
+
+--
+-- Indexes for table `django_migrations`
+--
+ALTER TABLE `django_migrations`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `django_session`
+--
+ALTER TABLE `django_session`
+ ADD PRIMARY KEY (`session_key`), ADD KEY `django_session_expire_date_a5c62663` (`expire_date`);
+
+--
+-- Indexes for table `polls_choice`
+--
+ALTER TABLE `polls_choice`
+ ADD PRIMARY KEY (`id`), ADD KEY `polls_choice_question_id_c5b4b260_fk_polls_question_id` (`question_id`);
+
+--
+-- Indexes for table `polls_question`
+--
+ALTER TABLE `polls_question`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `auth_group`
+--
+ALTER TABLE `auth_group`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `auth_permission`
+--
+ALTER TABLE `auth_permission`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+--
+-- AUTO_INCREMENT dla tabeli `auth_user`
+--
+ALTER TABLE `auth_user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT dla tabeli `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT dla tabeli `django_content_type`
+--
+ALTER TABLE `django_content_type`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT dla tabeli `django_migrations`
+--
+ALTER TABLE `django_migrations`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT dla tabeli `polls_choice`
+--
+ALTER TABLE `polls_choice`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT dla tabeli `polls_question`
+--
+ALTER TABLE `polls_question`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+ADD CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+ADD CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`);
+
+--
+-- Ograniczenia dla tabeli `auth_permission`
+--
+ALTER TABLE `auth_permission`
+ADD CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
+
+--
+-- Ograniczenia dla tabeli `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+ADD CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+ADD CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Ograniczenia dla tabeli `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+ADD CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+ADD CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Ograniczenia dla tabeli `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+ADD CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+ADD CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Ograniczenia dla tabeli `polls_choice`
+--
+ALTER TABLE `polls_choice`
+ADD CONSTRAINT `polls_choice_question_id_c5b4b260_fk_polls_question_id` FOREIGN KEY (`question_id`) REFERENCES `polls_question` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
