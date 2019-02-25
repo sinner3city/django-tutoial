885d4febb7214fd60e9970f8df4437b356a4a57c
Krystian Wojda <krystian.wojda@blueservices.pl>
2019-02-25 13:27:54 +0100

    tutek admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-
+from django.utils import timezone
 from .models import Question, Choice
 
 # Register your models here.
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'datepublished')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 10
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from django.db import models
 from django.utils import timezone
@@ -11,8 +11,13 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
+
+    def datepublished(self):
+        return self.pub_date.strftime('%B %d %Y')
 
     def __str__(self):
         return self.question_text
